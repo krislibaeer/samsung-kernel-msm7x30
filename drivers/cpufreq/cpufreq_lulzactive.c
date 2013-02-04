717d1c7a2f11ec2e9be0c1142ac00189f1f247bc
krislibaeer <krislibaeer@googlemail.com>
2013-02-04 20:54:59 +0100

    change governor

--- drivers/cpufreq/cpufreq_lulzactive.c
+++ drivers/cpufreq/cpufreq_lulzactive.c
@@ -75,7 +75,7 @@ static spinlock_t down_cpumask_lock;
 /*
  * The minimum amount of time to spend at a frequency before we can step up.
  */
-#define DEFAULT_UP_SAMPLE_TIME 20000
+#define DEFAULT_UP_SAMPLE_TIME 18000
 static unsigned long up_sample_time;
 
 #define DEFAULT_UP_SAMPLE_TIME_SLEEP 50000
@@ -84,7 +84,7 @@ static unsigned long up_sample_time_awake;
 /*
  * The minimum amount of time to spend at a frequency before we can step down.
  */
-#define DEFAULT_DOWN_SAMPLE_TIME 40000
+#define DEFAULT_DOWN_SAMPLE_TIME 55000
 static unsigned long down_sample_time;
 
 #define DEFAULT_DOWN_SAMPLE_TIME_SLEEP 40000
@@ -106,7 +106,7 @@ enum {
 /*
  * CPU freq will be increased if measured load > inc_cpu_load;
  */
-#define DEFAULT_INC_CPU_LOAD 70
+#define DEFAULT_INC_CPU_LOAD 65
 static unsigned long inc_cpu_load;
 
 #define DEFAULT_INC_CPU_LOAD_SLEEP 95
@@ -257,10 +257,10 @@ static inline void fix_screen_off_min_step(struct cpufreq_lulzactive_cpuinfo *pc
 	}
     
 	if (DEFAULT_SCREEN_OFF_MIN_STEP == screen_off_min_step) 
-		screen_off_min_step = pcpu->freq_table_size - 4;
+		screen_off_min_step = pcpu->freq_table_size - 5;
     
 	if (screen_off_min_step >= pcpu->freq_table_size)
-		screen_off_min_step = pcpu->freq_table_size - 4;
+		screen_off_min_step = pcpu->freq_table_size - 5;
 }
 
 static inline unsigned int adjust_screen_off_freq(
@@ -384,6 +384,8 @@ static void cpufreq_lulzactive_timer(unsigned long data)
 			index += pump_up_step;
 			if (index < 0)
 				index = 0;
+			if (index >= pcpu->freq_table_size)
+				index = pcpu->freq_table_size - 1;
 			new_freq = pcpu->freq_table[index].frequency;
 		}
 		else {
@@ -413,10 +415,10 @@ static void cpufreq_lulzactive_timer(unsigned long data)
 			}
 			// apply pump_down_step by tegrak
 			index -= pump_down_step;
-			if (index >= pcpu->freq_table_size) {
+			if (index < 0)
+				index = 0;
+			if (index >= pcpu->freq_table_size)
 				index = pcpu->freq_table_size - 1;
-			}
-            
 			new_freq = (pcpu->policy->cur > pcpu->policy->min) ? 
             (pcpu->freq_table[index].frequency) :
             (pcpu->policy->min);
