aa039af8134ac1184fdd93e3f5c7605304b74ff7
krislibaeer <krislibaeer@googlemail.com>
2013-02-11 14:55:55 +0100

    some changes

---
+++ include/linux/zerojpegcompression.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2012  Stefano Gottardo (alias CastagnaIT)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+
+#ifndef _LINUX_ZEROJPEGCOMPRESSION_H
+#define _LINUX_ZEROJPEGCOMPRESSION_H
+
+extern int zero_jpeg_compression;
+
+#endif
+
